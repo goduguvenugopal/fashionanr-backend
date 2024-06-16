5555f1941832904f570f633dfc8b0d9c48f2af29
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-06-16 13:04:13 +0530

    add

--- router/orderRouter.js
+++ router/orderRouter.js
@@ -2,10 +2,10 @@ const express = require("express")
 const router = express.Router()
 const orderController = require("../controller/orderController")
 
-router.post("add-order", orderController.saveOrder)
-router.get("get-all-orders/:userId",orderController.getAllOrders)
-router.get("get-single-order/:id" , orderController.getSingleOrder)
-router.delete("delete-order/:id",orderController.deleteOrder)
+router.post("/add-order", orderController.saveOrder)
+router.get("/get-all-orders/:userId",orderController.getAllOrders)
+router.get("/get-single-order/:id" , orderController.getSingleOrder)
+router.delete("/delete-order/:id",orderController.deleteOrder)
 
 
 module.exports = router
